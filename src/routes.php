f6956dde9ac3eab52033d9a51032b5d5c26e52c1
Gustavo Barragan <dev@droni.co>
2021-12-06 22:40:29 -0500

    route middleware

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,8 @@
 
 use Favik\Favik\Http\Controllers\AuthController;
 
-Route::get('/auth/login/redirect', [AuthController::class, 'redirect'])->name('login.redirect');
-Route::get('/auth/callback', [AuthController::class, 'callback']);
-Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
+Route::group(['middleware' => ['web']], function () {
+  Route::get('/auth/login/redirect', [AuthController::class, 'redirect'])->name('login.redirect');
+  Route::get('/auth/callback', [AuthController::class, 'callback']);
+  Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
+});
