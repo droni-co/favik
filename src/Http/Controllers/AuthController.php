6aa511405c9e47ae2b2bce2a90ace59dd019c5e0
Gustavo Barragan <dev@droni.co>
2021-12-06 22:19:30 -0500

    routes and controller

---
+++ src/Http/Controllers/AuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Favik\Favik\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+use App\Models\User;
+use Auth;
+
+class AuthController extends Controller
+{
+  public function login() {
+    return view('auth.login');
+  }
+  public function redirect(Request $request){
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => env("FAVIK_CLIENT_ID"),
+        'redirect_uri' => env("FAVIK_CALLBACK_URL"),
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+    ]);
+    return redirect('https://auth.favik.dev/oauth/authorize?'.$query);
+  }
+  public function callback(Request $request) {
+    $state = $request->session()->pull('state');
+
+    throw_unless(
+      strlen($state) > 0 && $state === $request->state,
+      InvalidArgumentException::class
+    );
+
+    $response = Http::asForm()->post('https://auth.favik.dev/oauth/token', [
+      'grant_type' => 'authorization_code',
+      'client_id' => env("FAVIK_CLIENT_ID"),
+      'client_secret' => env("FAVIK_CLIENT_SECRET"),
+      'redirect_uri' => env("FAVIK_CALLBACK_URL"),
+      'code' => $request->code,
+    ]);
+
+    $token = $response->json();
+    $favikUser = $this->getUser($token['access_token']);
+    //login or create user
+    $user = User::where('email', $favikUser['email'])->first();
+    if(!$user) {
+      $user = new User;
+      $user->email = $favikUser['email'];
+      $user->password = Str::random(40);
+      $user->email_verified_at = $favikUser['email_verified_at'];
+    }
+    $user->favik_token = json_encode($token);
+    $user->admin = $favikUser['admin'];
+    $user->name = $favikUser['name'];
+    $user->status = 'active';
+    $user->avatar = $favikUser['avatar'];
+
+    $user->save();
+
+    Auth::login($user, true);
+
+    return redirect()->route('home');
+  }
+
+  private function getUser($token) {
+    $response = Http::withToken($token)
+      ->get('https://auth.favik.dev/api/user');
+    return $response->json();
+  }
+
+  public function logout() {
+    $token = json_decode(Auth::user()->favik_token);
+    try {
+      Http::withToken($token->access_token)
+        ->post('https://auth.favik.dev/oauth/revoke');
+    } catch (\Exception $e) {
+      //
+    }
+    Auth::logout();
+    return redirect()->route('home');
+  }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,22 @@
 <?php
 
-Route::get('/favik', function(){
-	echo 'Hello from the calculator package!';
+use Favik\Favik\Http\Controllers\AuthController;
+
+Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
+Route::get('/auth/login/redirect', [AuthController::class, 'redirect'])->name('login.redirect');
+Route::get('/auth/callback', [AuthController::class, 'callback']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+  Route::resource('merchants', MerchantController::class)->except(['show']);
+  Route::resource('roles', RoleController::class)->only(['index', 'create', 'store', 'destroy']);
+});
+
+Route::middleware(['auth'])->group(function () {
+  Route::get('/', [WebController::class, 'index'])->name('home');
+  Route::get('/orders/{merchant_id}/list', [OrderController::class, 'list'])->name('orders.list');
+  Route::get('/orders/{merchant_id}/charts', [OrderController::class, 'charts'])->name('orders.charts');
+  Route::get('/analytics/{merchant_id}', [AnalyticsController::class, 'report'])->name('analytics.report');
+  Route::get('/users/{merchant_id}', [UserController::class, 'report'])->name('users.report');
 });
