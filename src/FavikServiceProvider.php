d99946f4d16239b1a66df9090203d4a0330f7e88
Gustavo Barragan <dev@droni.co>
2021-12-06 23:16:07 -0500

    database publish

--- src/FavikServiceProvider.php
+++ src/FavikServiceProvider.php
@@ -13,7 +13,7 @@ class FavikServiceProvider extends ServiceProvider
      */
     public function register()
     {
-      $this->mergeConfigFrom(__DIR__.'/config/config.php', 'favik');
+      $this->mergeConfigFrom(__DIR__.'/config/database.php', 'favik');
     }
 
     /**
