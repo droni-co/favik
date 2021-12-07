ac7b0a491f2a71957019383dc7a457d6bfec5fe5
Gustavo Barragan <dev@droni.co>
2021-12-06 22:34:46 -0500

    no login route

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,6 @@
 
 use Favik\Favik\Http\Controllers\AuthController;
 
-Route::get('/auth/login', [AuthController::class, 'login'])->name('login');
 Route::get('/auth/login/redirect', [AuthController::class, 'redirect'])->name('login.redirect');
 Route::get('/auth/callback', [AuthController::class, 'callback']);
 Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
