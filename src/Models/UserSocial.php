1f7bbcd2edb9f448bc2970357cf4d59d3bfdd91f
Gustavo Barragan <dev@droni.co>
2021-12-17 09:22:57 -0500

    remove autenticable

--- src/Models/UserSocial.php
+++ src/Models/UserSocial.php
@@ -6,26 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserSocial extends Model  {
 
-    use Authenticatable;
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $connection = 'mysql_favik';
+  protected $table = 'users_social';
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $connection = 'mysql_favik';
-    protected $table = 'users_social';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['username', 'avatar', 'email', 'provider', 'provider_id', 'user_id'];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = ['username', 'avatar', 'email', 'provider', 'provider_id', 'user_id'];
 
 
-    public function user() {
-        return $this->belongsTo('App\Models\User');
-    }
+  public function user() {
+      return $this->belongsTo('App\Models\User');
+  }
 
 }
