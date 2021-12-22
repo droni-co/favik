0363cd60e734e53537f543188c024ca00e4273ad
Daniel Esteban Jiménez Morales <djimenezmo3@uniminuto.edu.co>
2021-12-22 15:25:09 -0500

    add post, put, patch and delete dynamic functions

--- src/Http/Controllers/GatewayController.php
+++ src/Http/Controllers/GatewayController.php
@@ -9,18 +9,48 @@ use Auth;
 
 class GatewayController extends Controller
 {
-  public static function get($app, $endpoint, $params=[]) {
+  public static function get($app, $endpoint) {
     $token = Auth::user()->favik_token;
     $url = $this->app($app).$endpoint;
-    $response = Http::withToken($token)->get($url, $params);
+    $response = Http::withToken($token)->get($url);
     return $response;
   }
+
+  public static function post($app, $endpoint, $params=[]) {
+    $token = Auth::user()->favik_token;
+    $url = $this->app($app).$endpoint;
+    $response = Http::withToken($token)->post($url, $params);
+    return $response;
+  }
+
+  public static function putUpdate($app, $endpoint, $params=[]) {
+    $token = Auth::user()->favik_token;
+    $url = $this->app($app).$endpoint;
+    $response = Http::withToken($token)->put($url, $params);
+    return $response;
+  }
+
+  public static function patchUpdate($app, $endpoint, $params=[]) {
+    $token = Auth::user()->favik_token;
+    $url = $this->app($app).$endpoint;
+    $response = Http::withToken($token)->patch($url, $params);
+    return $response;
+  }
+
+  public static function delete($app, $endpoint) {
+    $token = Auth::user()->favik_token;
+    $url = $this->app($app).$endpoint;
+    $response = Http::withToken($token)->delete($url);
+    return $response;
+  }
+
   private static function app($app) {
     $res = [
       'archimedes' => 'https://archimedes.favik.dev',
       'attribution' => 'https://attribution.favik.dev',
       'reports' => 'https://reports.favik.dev',
     ];
+
     return $res[$app];
   }
 }
